f6616f624e10fcd7984724b6e61a101be79f8c26
mellowmorgan <morgan.waites@gmail.com>
2021-11-02 10:43:46 -0700

    Add UI logic
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,31 @@ BankAccount.prototype.withdraw = function(amount) {
 BankAccount.prototype.deposit = function(amount) {
     this.currentBalance = this.currentBalance + amount;
 }
+
+function displayBalance(account){
+    let currentBalance = account.currentBalance;
+    $("#balance").text(currentBalance);
+}
+
+$(document).ready(function(){
+    let account;
+    $("#formOne").submit(function(event){
+        event.preventDefault();
+        const name = $("#name").val();
+        const initialDeposit = parseInt($("#initial-deposit").val());
+        account = new BankAccount(name, initialDeposit);
+        displayBalance(account);
+    });
+    $("#formTwo").submit(function(event){
+        event.preventDefault();
+        let deposit = parseInt($("#deposit").val());
+        let withdrawal = parseInt($("#withdrawal").val());
+        if(withdrawal){
+            account.withdraw(withdrawal);
+        }
+        if(deposit){
+            account.deposit(deposit);
+        }
+        displayBalance(account);
+    });
+});
